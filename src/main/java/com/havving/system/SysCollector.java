60a8bcc757abb4b7c81b3c2f69638ce4bf866a1f
havving <sparkle5406@gmail.com>
2021-03-02 23:34:15 +0900

    0302 commit

---
+++ src/main/java/com/havving/system/SysCollector.java
@@ -0,0 +1,23 @@
+package com.havving.system;
+
+/**
+ * Created by HAVVING on 2021-03-02.
+ */
+
+
+import com.sun.org.slf4j.internal.Logger;
+import com.sun.org.slf4j.internal.LoggerFactory;
+
+
+/**
+ * SysCollector Main Class
+ */
+public class SysCollector {
+    private static final Logger log = LoggerFactory.getLogger(SysCollector.class);
+
+    public static void main(String[] args) {
+        // Initialize
+
+
+    }
+}
