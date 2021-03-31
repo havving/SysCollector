7f4c40472da843dc923890df5229b7438397e2bd
havving <sparkle5406@gmail.com>
2021-03-31 22:55:20 +0900

    0331 ORMConnection

--- src/main/java/com/havving/system/service/batch/JobExecutor.java
+++ src/main/java/com/havving/system/service/batch/JobExecutor.java
@@ -1,14 +1,10 @@
 package com.havving.system.service.batch;
 
-import com.havving.system.global.Constants;
+import com.havving.system.global.ORMConnection;
 import lombok.extern.slf4j.Slf4j;
-import org.quartz.*;
-import org.quartz.impl.StdSchedulerFactory;
-
-import java.util.Date;
-
-import static org.quartz.CronScheduleBuilder.cronSchedule;
-import static org.quartz.TriggerBuilder.newTrigger;
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
 
 /**
  * Created by HAVVING on 2021-03-28.
@@ -17,6 +13,8 @@ import static org.quartz.TriggerBuilder.newTrigger;
  */
 @Slf4j
 public class JobExecutor implements Job {
+
+    private ORMConnection connection = null;
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
